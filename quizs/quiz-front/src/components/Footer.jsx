a8b3a18801dc20454f1feaa2f0d66dfd1eba50da
Eng1Mahmoud <mahmoudmohamedabdelaal1@gmail.com>
2023-09-25 23:42:33 +0300

    chang style

--- quizs/quiz-front/src/components/Footer.jsx
+++ quizs/quiz-front/src/components/Footer.jsx
@@ -1,11 +1,9 @@
 const Footer = () => {
-    const currentYear = new Date().getFullYear();
-  
+
     return (
       <footer>
         <div className="container">
-          <p>&copy; {currentYear} Quiz. All Rights Reserved.</p>
-          <p> 👨‍💻 developed by <a href="https://www.linkedin.com/in/mahmoud-mohamed-abdel-aal">Mahmoud Mohamed</a> </p>
+      👨‍💻 developed by <a href="https://www.linkedin.com/in/mahmoud-mohamed-abdel-aal">Mahmoud Mohamed</a>
         </div>
       </footer>
     );
