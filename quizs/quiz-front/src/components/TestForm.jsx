58768d3f30d57d09845d5a7aa2e0ae5988ec88fd
Eng1Mahmoud <mahmoudmohamedabdelaal1@gmail.com>
2023-09-26 22:23:46 +0300

    update some logic

--- quizs/quiz-front/src/components/TestForm.jsx
+++ quizs/quiz-front/src/components/TestForm.jsx
@@ -1,169 +1,202 @@
-import  { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import testImage from "../assets/test.png";
 import axios from "axios";
 import Cookies from "js-cookie";
-import { useNavigate } from "react-router-dom";
-import { AiOutlineClose } from "react-icons/ai";
+import {useNavigate} from "react-router-dom";
+import {AiOutlineClose} from "react-icons/ai";
 
 const TestForm = () => {
-  const navigate = useNavigate();
-  const [testName, setTestName] = useState("");
-  const [questions, setQuestions] = useState([]);
-  const [currentQuestion, setCurrentQuestion] = useState({
-    question: "",
-    options: ["", "", "", ""],
-    correctAnswer: "",
-  });
-  const [loading, setLoading] = useState(false);
-  const [addedTest, setAddedTest] = useState({ added: false, message: "",error:false });
-
-  const closePopup = () => {
-    setAddedTest({ added: false, message: "" });
-  };
-
-  const handleTestNameChange = (event) => {
-    setTestName(event.target.value);
-  };
-
-  const handleQuestionChange = (event) => {
-    setCurrentQuestion({
-      ...currentQuestion,
-      question: event.target.value,
+    const navigate = useNavigate();
+    const [testName, setTestName] = useState("");
+    const [questions, setQuestions] = useState([]);
+    const [currentQuestion, setCurrentQuestion] = useState({
+        question: "",
+        options: [
+            "", "", "", ""
+        ],
+        correctAnswer: ""
     });
-  };
+    const [loading, setLoading] = useState(false);
+    const [addedTest, setAddedTest] = useState({added: false, message: "", error: false});
 
-  const handleOptionChange = (optionIndex, event) => {
-    const updatedOptions = [...currentQuestion.options];
-    updatedOptions[optionIndex] = event.target.value;
-    setCurrentQuestion({
-      ...currentQuestion,
-      options: updatedOptions,
-    });
-  };
+    const closePopup = () => {
+        setAddedTest({added: false, message: ""});
+    };
 
-  const handleCorrectAnswerChange = (event) => {
-    setCurrentQuestion({
-      ...currentQuestion,
-      correctAnswer: event.target.value,
-    });
-  };
+    const handleTestNameChange = (event) => {
+        setTestName(event.target.value);
+    };
 
-  const addQuestion = () => {
-    const trimmedQuestion = currentQuestion.question.trim();
-    const trimmedOptions = currentQuestion.options.map(option => option.trim());
+    const handleQuestionChange = (event) => {
+        setCurrentQuestion({
+            ...currentQuestion,
+            question: event.target.value
+        });
+    };
 
-    // Check if the question and all options are entered
-    if (trimmedQuestion === "" || trimmedOptions.some(option => option === "")) {
-      setAddedTest({ added: true, message: "Please Enter Question or Options",error:true });
-      return;
-    }
+    const handleOptionChange = (optionIndex, event) => {
+        const updatedOptions = [...currentQuestion.options];
+        updatedOptions[optionIndex] = event.target.value;
+        setCurrentQuestion({
+            ...currentQuestion,
+            options: updatedOptions
+        });
+    };
 
-    setQuestions([...questions, { ...currentQuestion }]);
-    setCurrentQuestion({
-      question: "",
-      options: ["", "", "", ""],
-      correctAnswer: "",
-    });
-  };
+    const handleCorrectAnswerChange = (event) => {
+        setCurrentQuestion({
+            ...currentQuestion,
+            correctAnswer: event.target.value
+        });
+    };
 
-  const handleSubmit = async (e) => {
-    setLoading(true);
-    e.preventDefault();
+    const addQuestion = () => {
+        const trimmedOptions = currentQuestion.options.map(option => option.trim());
 
-    // Check if test name is empty
-    if (testName.trim() === "") {
-    setAddedTest({ added: true, message: "Please enter a test name.",error:true })
-    
-      setLoading(false);
-      return;
-    }
-
-    // Check if there are no questions
-    if (questions.length === 0) {
-        setAddedTest({ added: true, message: "Please add at least one question.",error:true })
-      setLoading(false);
-      return;
-    }
-    try {
-      const res = await axios.post(
-        "https://quizs-wg02.onrender.com/api/addTest",
-        { testName, questions },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${Cookies.get("token")}`,
-          },
+        // Check if the question and all options are entered
+        if (currentQuestion.question === "" || trimmedOptions.some(option => option === "")) {
+            setAddedTest({added: true, message: "Please Enter Question or Options", error: true});
+            return;
         }
-      );
 
-      setTestName("");
-      setAddedTest({ added: res.data.added, message: res.data.message });
-      setLoading(false);
-    } catch (error) {
-      console.error("Error:", error.message);
-    }
-  };
+        // Check if the correct answer exists in the options
+        const trimmedCorrectAnswer = currentQuestion.correctAnswer.trim();
+        if (! trimmedOptions.includes(trimmedCorrectAnswer)) {
+            setAddedTest({added: true, message: "Please ensure the correct answer exists in the options.", error: true});
+            return;
+        }
 
-  useEffect(() => {
-    if (Cookies.get("token") === undefined || !Cookies.get("token")) {
-      navigate("/signin");
-    }
-  }, [navigate]);
+        setQuestions([
+            ...questions, {
+                question: currentQuestion.question,
+                options: trimmedOptions,
+                correctAnswer: trimmedCorrectAnswer
+            }
+        ]);
+        setCurrentQuestion({
+            question: "",
+            options: [
+                "", "", "", ""
+            ],
+            correctAnswer: ""
+        });
+    };
 
-  return (
-    <div className="container-test">
-      {addedTest.added ? (
-        <div className="popup" style={{ background: addedTest.error?"red":"green" }}>
-          <p>{addedTest.message}</p>
-          <AiOutlineClose className="close" onClick={closePopup} />
+    const handleSubmit = async (e) => {
+        setLoading(true);
+        e.preventDefault();
+
+        // Check if test name is empty
+        if (testName === "") {
+            setAddedTest({added: true, message: "Please enter a test name.", error: true});
+            setLoading(false);
+            return;
+        }
+
+        // Check if there are no questions
+        if (questions.length === 0) {
+            setAddedTest({added: true, message: "Please add at least one question.", error: true});
+            setLoading(false);
+            return;
+        }
+
+        try {
+            const res = await axios.post("https://quizs-wg02.onrender.com/api/addTest", {
+                testName,
+                questions
+            }, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${
+                        Cookies.get("token")
+                    }`
+                }
+            });
+
+            setTestName("");
+            setAddedTest({added: res.data.added, message: res.data.message});
+            setLoading(false);
+        } catch (error) {
+            console.error("Error:", error.message);
+        }
+    };
+
+    useEffect(() => {
+        if (Cookies.get("token") === undefined || !Cookies.get("token")) {
+            navigate("/signin");
+        }
+    }, [navigate]);
+
+    return (
+        <div className="container-test">
+            {
+            addedTest.added ? (
+                <div className="popup"
+                    style={
+                        {
+                            background: addedTest.error ? "red" : "green"
+                        }
+                }>
+                    <p>{
+                        addedTest.message
+                    }</p>
+                    <AiOutlineClose className="close"
+                        onClick={closePopup}/>
+                </div>
+            ) : null
+        }
+            <div className="image">
+                <img src={testImage}
+                    alt="test"
+                    loading="lazy"/>
+            </div>
+            <div className="question-form-container">
+                <form onSubmit={handleSubmit}
+                    className="question-form">
+                    <input type="text" placeholder="Test Name"
+                        value={testName}
+                        onChange={handleTestNameChange}
+                        className="input-field"/>
+                    <input type="text" placeholder="Question"
+                        value={
+                            currentQuestion.question
+                        }
+                        onChange={handleQuestionChange}
+                        className="input-field"/> {
+                    currentQuestion.options.map((option, optionIndex) => (
+                        <input key={optionIndex}
+                            type="text"
+                            placeholder={
+                                `Option ${
+                                    optionIndex + 1
+                                }`
+                            }
+                            value={option}
+                            onChange={
+                                (e) => handleOptionChange(optionIndex, e)
+                            }
+                            className="input-field"/>
+                    ))
+                }
+                    <input type="text" placeholder="Correct Answer"
+                        value={
+                            currentQuestion.correctAnswer
+                        }
+                        onChange={handleCorrectAnswerChange}
+                        className="input-field"/>
+                    <button type="button"
+                        onClick={addQuestion}
+                        className="action-button">
+                        Add Question
+                    </button>
+                    <button type="submit" className="action-button">
+                        {
+                        loading ? "Loading..." : "ADD TEST"
+                    } </button>
+                </form>
+            </div>
         </div>
-      ) : null}
-      <div className="image">
-        <img src={testImage} alt="test" loading="lazy"/>
-      </div>
-      <div className="question-form-container">
-        <form onSubmit={handleSubmit} className="question-form">
-          <input
-            type="text"
-            placeholder="Test Name"
-            value={testName}
-            onChange={handleTestNameChange}
-            className="input-field"
-          />
-          <input
-            type="text"
-            placeholder="Question"
-            value={currentQuestion.question}
-            onChange={handleQuestionChange}
-            className="input-field"
-          />
-          {currentQuestion.options.map((option, optionIndex) => (
-            <input
-              key={optionIndex}
-              type="text"
-              placeholder={`Option ${optionIndex + 1}`}
-              value={option}
-              onChange={(e) => handleOptionChange(optionIndex, e)}
-              className="input-field"
-            />
-          ))}
-          <input
-            type="text"
-            placeholder="Correct Answer"
-            value={currentQuestion.correctAnswer}
-            onChange={handleCorrectAnswerChange}
-            className="input-field"
-          />
-          <button type="button" onClick={addQuestion} className="action-button">
-            Add Question
-          </button>
-          <button type="submit" className="action-button">
-            {loading ? "Loading..." : "ADD TEST"}
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default TestForm;
--- quizs/quiz-front/src/components/TestList.jsx
+++ quizs/quiz-front/src/components/TestList.jsx
@@ -4,16 +4,18 @@ import Cookies from "js-cookie";
 import { AiFillTwitterCircle } from "react-icons/ai";
 import { AiFillLinkedin } from "react-icons/ai";
 import { BiLogoGmail } from "react-icons/bi";
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
 const TestList = () => {
   const navigate = useNavigate();
   const [tests, setTests] = useState([]);
-console.log(tests)
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     if (Cookies.get("token") === undefined || !Cookies.get("token")) {
       navigate("/signin");
     }
     const fetchTests = async () => {
+      setLoading(true);
       try {
         const response = await axios.get(
           "https://quizs-wg02.onrender.com/api/getTests",
@@ -25,6 +27,7 @@ console.log(tests)
         );
 
         setTests(response.data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching tests:", error);
       }
@@ -38,15 +41,17 @@ console.log(tests)
       <div className="content">
         <h1>Test List</h1>
         <div className="testList">
-          {tests.length === 0 ? (
+          {loading ? (
+            <h2>Loading...</h2>
+          ) : tests.length === 0 ? (
             <h2>No Tests Available</h2>
           ) : (
             tests.map((test) => (
               <div className="test" key={test._id}>
                 <h3 className="title">{test.testName}</h3>
                 <div className="publisher">
-                  <p>Published by {test.publisher.fullName}</p> 
-                 <div className="contact">
+                  <p>Published by {test.publisher.fullName}</p>
+                  <div className="contact">
                     <a
                       href={test.publisher.twitter}
                       target="_blank"
@@ -68,7 +73,7 @@ console.log(tests)
                     >
                       <AiFillLinkedin />
                     </a>
-                  </div> 
+                  </div>
                 </div>
 
                 <button onClick={() => navigate(`/taketest/${test._id}`)}>
@@ -76,7 +81,7 @@ console.log(tests)
                 </button>
               </div>
             ))
-          )}{" "}
+          )}
         </div>
       </div>
     </div>
